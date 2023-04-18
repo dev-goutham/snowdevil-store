f3f4f77e7b6aee192978e4eaa46032a2af711c21
dev-goutham <dev.gouthamram95@gmail.com>
2023-04-18 14:08:37 +0530

    added fixed height to ProductCard to handle edgecases

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -6,6 +6,7 @@ import {getDescription} from '~/utils/getDescription';
 
 interface Props {
   product: Product;
+  size?: 'large' | 'medium' | 'small';
 }
 
 const ProductCard: React.FC<Props> = ({product}) => {
@@ -16,22 +17,22 @@ const ProductCard: React.FC<Props> = ({product}) => {
       to={`/product/${product.handle}`}
       className="flex flex-col gap-4 p-4 border border-gray-200 shadow-md shrink-0"
     >
-      <div className="flex items-center h-full">
+      <div className="flex items-center justify-center max-h-[410px] min-h-[410px]">
         <Image
-          className="block w-72"
+          className="block w-72 max-h-[410px] object-cover"
           data={{
             url: product.featuredImage!.url,
             altText: product.featuredImage?.altText || '',
-            height: 320,
+            height: 410,
             width: 320,
           }}
         />
       </div>
       <hr className="my-2" />
-      <div className="flex flex-col w-72">
+      <div className="flex flex-col min-h-[128px] w-72">
         <h3 className="mb-1 font-semibold text-gray-600">{product.title}</h3>
         <p className="mb-3 text-sm tracking-wider text-gray-500 line-clamp-3">
-          {getDescription(product.description)}
+          {product.seo.description || getDescription(product.description)}
         </p>
         <p className="text-lg font-semibold text-indigo-700">{amount}</p>
       </div>
