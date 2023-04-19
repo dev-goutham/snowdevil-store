97994dbd37d5da5d422e806529f76019bc30bdc2
dev-goutham <dev.gouthamram95@gmail.com>
2023-04-19 12:55:10 +0530

    added Filters component to collections page and made necessary changes to loader

--- app/routes/collections.$handle.tsx
+++ app/routes/collections.$handle.tsx
@@ -1,14 +1,21 @@
 import {useLoaderData, useSearchParams} from '@remix-run/react';
 import {LoaderArgs} from '@shopify/remix-oxygen';
 import {getCollectionProducts} from '~/api/getCollectionProduts.server';
-import Filters from '~/components/Filters';
+import Filters from '~/components/Filter';
 import Pagination from '~/components/Pagination';
 import ProductCard from '~/components/ProductCard';
 
 const CollectionsPage: React.FC = () => {
   const [searchParams] = useSearchParams();
-  const {products, productCount, pageInfo, filters} =
-    useLoaderData() as Awaited<ReturnType<typeof loader>>;
+  const {
+    products,
+    productCount,
+    pageInfo,
+    filters,
+    selectedFilters,
+    priceRange,
+    selectedPriceRange,
+  } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
 
   const currentPage = searchParams.get('page') || '1';
   const numberOfPages = Math.ceil(productCount / 9);
@@ -17,7 +24,12 @@ const CollectionsPage: React.FC = () => {
 
   return (
     <div className="flex flex-col my-12 app-container lg:justify-between lg:flex-row lg:my-24">
-      <Filters filters={filters} />
+      <Filters
+        priceRange={priceRange}
+        selectedFilters={selectedFilters}
+        filters={filters}
+        selectedPriceRange={selectedPriceRange}
+      />
       <div>
         <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:gap-x-8 xl:grid-cols-3">
           {products.map((product) => (
@@ -40,6 +52,19 @@ export default CollectionsPage;
 export const loader = async ({params, context, request}: LoaderArgs) => {
   const handle = params.handle || 'all';
 
+  const searchParams = new URLSearchParams(request.url.split('?')[1]).entries();
+  const selectedFilters: string[] = [];
+  let selectedPriceRange: [number, number] | null = null;
+  Array.from(searchParams).forEach((param) => {
+    if (param[0] === 'priceRange') {
+      const [min, max] = param[1].split('-');
+      selectedPriceRange = [Number(min), Number(max)];
+    } else {
+      const str = JSON.stringify({[`${param[0]}`]: param[1]});
+      selectedFilters.push(str);
+    }
+  });
+
   const pageNumber = Number(
     new URLSearchParams(request.url.split('?')[1]).get('page') || 1,
   );
@@ -50,6 +75,16 @@ export const loader = async ({params, context, request}: LoaderArgs) => {
       handle,
       pageNumber,
     });
+
+    const priceRange = JSON.parse(
+      products.filters.find((filter) => filter.label === 'Price')!.values[0]
+        .input as string,
+    ) as {
+      price: {
+        min: number;
+        max: number;
+      };
+    };
 
     const token = context.env.SHOPIFY_ADMIN_TOKEN;
     const storeUrl = context.env.PUBLIC_STORE_DOMAIN;
@@ -78,8 +113,12 @@ export const loader = async ({params, context, request}: LoaderArgs) => {
       products: products.nodes,
       productCount: res.collection.products_count,
       filters: products.filters,
+      selectedFilters,
+      selectedPriceRange,
+      priceRange,
     };
   } catch (error) {
-    throw new Error('fuck');
+    console.log(error);
+    throw new Error(JSON.stringify(error));
   }
 };
